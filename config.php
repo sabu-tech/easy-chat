1db05fad7ef13a548a1790b3d0d498e212551ba3
sabu-tech <71729501+sabu-tech@users.noreply.github.com>
2020-10-24 10:55:30 +0900

    Update config.php

--- config.php
+++ config.php
@@ -2,6 +2,6 @@
 
 //ini_set('display_errors', 1);
 
-define('DSN', 'mysql:host=localhost;dbname=tb220600db');
-define('DB_USER', 'tb-220600');
-define('DB_PASS', 'e2Jrg7mBzm');
+define('DSN', 'mysql:host=localhost;dbname=******');
+define('DB_USER', '*****');
+define('DB_PASS', '*******');
